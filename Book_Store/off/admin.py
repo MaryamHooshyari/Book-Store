1d69f54e2283b4d2b9e61720a4c9784804dba010
MaryamHooshyari <mhshyr96@gmail.com>
2021-08-19 17:25:29 +0430

    Book_Store: source failed file deleted

--- Book_Store/off/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import BookOff, BonusOff
-
-admin.site.register(BookOff)
-admin.site.register(BonusOff)
--- Book_Store/product/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views.search import SearchResultsView
-
-urlpatterns = [
-    path('search/', SearchResultsView.as_view(), name='search_result')
-]
