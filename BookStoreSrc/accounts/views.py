5a6a4367321fc6bb41e1773cbd27b9c786df692c
MaryamHooshyari <mhshyr96@gmail.com>
2021-08-25 23:59:50 +0430

    phase three

--- BookStoreSrc/accounts/views.py
+++ BookStoreSrc/accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
+from product.models.book import Book
 from .forms import CustomUserCreationForm
 
 
@@ -14,7 +15,12 @@ class SignUpView(CreateView):
 def login_redirect(request):
     if request.user.is_superuser:
         return redirect('admin_panel')
-    elif not request.user.is_superuser and request.user.is_staff:
+    elif request.user.is_staff:
         return redirect('staff_panel')
     else:
         return redirect('home')
+
+
+class StaffHome(ListView):
+    model = Book
+    template_name = 'employee/staff_home.html'
